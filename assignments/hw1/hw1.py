beebf94f9036b158ebe00a38d41e7472b17a0f02
SteveHyland <hylandsl@g.cofc.edu>
2022-01-14 10:12:07 -0500

    HW 1 and test push

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,12 +1,10 @@
 """
 Name: <Steve Hyland>
-<Pro>.py
+HW1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Homework assignmet 1 - modify existing code and add code per assignment instructions>
 
-Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
+Certification of Authenticity: I certify that this assignment is entirely my own work.
 """
 
 # Provided Code - no changes to the code except deletion of the PASS statement
@@ -16,7 +14,7 @@ def calc_rec_area():
     area = length * width
     print("Area =", area)
 
-# First Coding Exercise - my changes to the code
+# First Coding Exercise - my changes to the code - compute volume length * width * depth.
 def calc_volume():
     length = eval(input("Enter the length: "))
     width = eval(input("Enter the width: "))
@@ -24,20 +22,20 @@ def calc_volume():
     vol = length * width * depth
     print("Volume =", vol)
 
-# Second coding exercise - calculate shooter's accuracy percentage
+# Second coding exercise - calculate shooter's accuracy percentage - shots made / attempts.
 def shooting_percentage():
     shots = eval(input("Enter the player's total shots: "))
     made = eval(input("Enter how many shots the player made: "))
     percentage = made / shots * 100
     print("Shooting Percentage =", percentage)
 
-# Third coding exercise - calculate total cost of a coffee order
+# Third coding exercise - calculate total cost of a coffee order with given defaults.
 def coffee():
     qty = eval(input("How many pounds of coffee would you like?: "))
     cost = 1.50 + (qty * 10.50) + (qty * 0.86)
     print("Your total is: ", cost)
 
-# Fourth coding exercise - convert kilosmeters to miles
+# Fourth coding exercise - convert kilosmeters to miles with formula provided.
 def kilometers_to_miles():
     kilometers = eval(input("How many kilometers did you travel?: "))
     miles = (kilometers / 1.61)
