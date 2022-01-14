76c30daacf5de782122df160c5d70489ba3d0704
SteveHyland <hylandsl@g.cofc.edu>
2022-01-14 09:08:46 -0500

    HW 1 and test push

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,62 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Steve Hyland>
+<Pro>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
+# Provided Code - no changes to the code except deletion of the PASS statement
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
-
+# First Coding Exercise - my changes to the code
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    depth = eval(input("Enter the depth: "))
+    vol = length * width * depth
+    print("Volume =", vol)
 
-
+# Second coding exercise - calculate shooter's accuracy percentage
 def shooting_percentage():
-    pass
+    shots = eval(input("Enter the player's total shots: "))
+    made = eval(input("Enter how many shots the player made: "))
+    percentage = made / shots * 100
+    print("Shooting Percentage =", percentage)
 
-
+# Third coding exercise - calculate total cost of a coffee order
 def coffee():
-    pass
+    qty = eval(input("How many pounds of coffee would you like?: "))
+    cost = 1.50 + (qty * 10.50) + (qty * 0.86)
+    print("Your total is: ", cost)
 
-
+# Fourth coding exercise - convert kilosmeters to miles
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel?: "))
+    miles = (kilometers / 1.61)
+    print("That's", miles, "miles!")
 
 
 if __name__ == '__main__':
     pass
+
+# instructions for running - uncomment the procedure you wish to test by removing the hashtag.
+
+calc_rec_area()
+
+calc_volume()
+
+shooting_percentage()
+
+coffee()
+
+kilometers_to_miles()
+
+
